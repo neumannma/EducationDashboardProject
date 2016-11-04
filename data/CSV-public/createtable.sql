05a20ceb324ae94b24669e1fae967bf3a0bf2bd4
Alex Martin <atmartin@scu.edu>
2016-11-04 16:36:18 -0700

    untracked deleted files

--- data/CSV-public/createtable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `2015` AS
-SELECT
-    `hc_key` AS 'hc-key',
-    `SEX` AS 'gender',
-    `DIV` AS 'division'
-FROM
-    newdata.`2015` JOIN newdata.SchoolsToRegion
-    ON newdata.`2015`.center_id = newdata.SchoolsToRegion.necta
-;
